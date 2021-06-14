98e48e533485b00d77a15ae647f92a5cfa039a6b
Ajit Inamdar <ajit.inamdar@blazeclan.com>
2021-06-14 11:51:32 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,14 @@
+
+module "s3_bucket" {
+  source  = "app.terraform.io/ajitinamdar-training/s3-bucket/aws"
+  version = "2.2.0"
+
+  bucket = "my-s3-bucket"
+  bucket_prefix = "ajitinamdar"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
